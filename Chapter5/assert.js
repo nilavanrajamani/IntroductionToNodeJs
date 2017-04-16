b6fdaff4cfdb5a068dd5ebf09916d22c92cbd25c
Nila <nilavanrajamani@gmail.com>
2017-04-16 07:31:06 +0000

    Sunday mid

---
+++ Chapter5/assert.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var fun = require("./mathfun");
+
+assert.equal(fun.evenDoublerSync(2),4);
+
+assert.throws(function(){
+    fun.evenDoublerSync(3);
+},/Odd/);
+
+fun.evenDoubler(2, function(err, results){
+   assert.ifError(err);
+   assert.equal(results,5,"evendoubler failed on even number");
+});
+
+fun.evenDoubler(3, function(err, results){
+    assert.notEqual(err, null);
+});
---
+++ Chapter5/mathfun.js
@@ -0,0 +1,28 @@
+var maxTime = 1000;
+
+var evenDoubler = function(v, callback){
+    var waitTime = Math.floor(Math.random()*(maxTime+1));
+    if(v%2){
+        setTimeout(function(){
+            callback(new Error("Odd Input"));
+        }, waitTime);
+    }
+    else{
+        setTimeout(function() {
+            callback(null, v*2, waitTime);
+        }, waitTime);
+    }
+};
+
+var evenDoublerSync = function(v){
+    if(v%2){
+        throw(new Error("Odd input"));
+    }else{
+        return(v*2);
+    }
+};
+
+module.exports.evenDoubler = evenDoubler;
+module.exports.evenDoublerSync = evenDoublerSync;
+
+module.exports.foo = 'bar';
